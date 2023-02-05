1ec4b90465570959284e884737410fa86d7e55a2
BhawnaMisra <124463977+BhawnaMisra@users.noreply.github.com>
2023-02-04 20:00:54 -0600

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,30 @@
+package week1.day1;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		
+		String mobileBrandName = "iPhone";
+		System.out.println(mobileBrandName);
+		
+		char mobileLogo = 'A';
+		System.out.println(mobileLogo);
+		
+		short noofMobilePiece = 8000;
+		System.out.println(noofMobilePiece);
+		
+		int modelNumber = 14;
+		System.out.println(modelNumber);
+		
+		long mobilemeiNumber = 12999025555513L;
+		System.out.println(mobilemeiNumber);
+		
+		float mobilePrice = 90000.456f;
+		System.out.println(mobilePrice);
+		
+
+		boolean isDamaged = false;
+		System.out.println(isDamaged);
+		
+	}
+}
---
+++ TwoWheeler.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class TwoWheeler {
+	public static void main(String[] args) {
+		
+		int noOfWheels = 4;
+		System.out.println(noOfWheels);
+		
+		short noOfMirrors = 2;
+		System.out.println(noOfMirrors);
+		
+		long chassIsNumber = 6995777l;
+		System.out.println(chassIsNumber);
+		
+		boolean isPunctured = true;
+		System.out.println(isPunctured);
+		
+		String bikeName = "Royal Enfield";
+		System.out.println(bikeName);
+		
+		double runningKM = 256666.1234;
+		System.out.println(runningKM);
+				
+		
+		
+		
+	}
+}
